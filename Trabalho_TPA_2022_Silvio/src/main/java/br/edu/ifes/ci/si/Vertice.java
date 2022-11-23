27ec1cc910de8a5403561055f869d950c98b2517
Sílvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-11-23 14:05:47 -0300

    Estrutura primaria de um vertice

---
+++ Trabalho_TPA_2022_Silvio/src/main/java/br/edu/ifes/ci/si/Vertice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifes.ci.si;
+
+import java.util.List;
+
+/**
+ *
+ * @author silvio
+ */
+public class Vertice {
+    private int vertice;
+    private int autor;
+    //private List<Aresta>[] adj;
+    
+    
+    
+}

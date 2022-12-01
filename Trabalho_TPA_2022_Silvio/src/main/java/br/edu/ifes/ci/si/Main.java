63b821976db8d75e90509b804a7ace6696783fcd
Silvio Cesar Silva Oliveira <silviocesar.silva2903@gmail.com>
2022-12-01 10:18:42 -0300

    Adicionando as funcoes printMenu e menu que serao responsaveis na execucao principal do projeto

--- Trabalho_TPA_2022_Silvio/src/main/java/br/edu/ifes/ci/si/Main.java
+++ Trabalho_TPA_2022_Silvio/src/main/java/br/edu/ifes/ci/si/Main.java
@@ -5,6 +5,8 @@
 
 package br.edu.ifes.ci.si;
 
+import java.util.Scanner;
+
 /**
  *
  * @author silvio
@@ -12,6 +14,63 @@ package br.edu.ifes.ci.si;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        In in = new In(args[0]);
+        int vo = Integer.parseInt(args[1]);
+        //System.out.println("Hello World!");
+        menu(in, vo);
+    }
+    
+    public static void menu(In in, int vo)
+    {
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
+        Digrafo G = new Digrafo(in);
+        do{
+            printMenu();
+            op = sc.nextInt();
+            switch(op){
+                case 1: 
+                    AlgoritmoBFSDigrafoMenorCaminho algoritmoBFS = new AlgoritmoBFSDigrafoMenorCaminho(G, vo);
+                    for (int v = 0; v < G.V(); v++) 
+                    {
+                        if (algoritmoBFS.temCaminhoPara(v)) {
+                            System.out.printf("%d para %d (%d):  ", vo, v, algoritmoBFS.distanciaPara(v));
+                            for (int x : algoritmoBFS.caminhoPara(v)) 
+                            {
+                                if (x == vo) {
+                                    System.out.print(x);
+                                } else {
+                                    System.out.print("->" + x);
+                                }
+                            }
+                            System.out.println();
+                        } else {
+                            System.out.printf("%d para %d (-):  não conectado\n", vo, v);
+                        }
+                    }
+                    break;
+                case 4:
+                    AlgoritimoTopAutores ata = new AlgoritimoTopAutores(G);
+                    ata.printaTopAutores();
+                    break;
+                case 5:
+                    System.out.println("Encerrendo o Sistemas!!!");
+                    op =5;
+                    break;
+            }
+        }while(op != 5);
+}
+    
+   
+    /**
+     *
+     */
+    public static void printMenu(){
+        System.out.println("Digite 1 para encontrar o menor caminho entre dois artigos");
+        System.out.println("Digite 2 para encontrar todos os caminhos entre dois artigos");
+        System.out.println("Digite 3 para o numero de citaçĩes de cada artigo");
+        System.out.println("Digite 3 para o numero de citaçĩes de cada autor");
+        System.out.println("Digite 5 para encerrar a aplicação");
+        System.out.println("\n Digite a opção desejada!!");
     }
 }
